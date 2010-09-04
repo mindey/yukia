a97f2b5af32b3067a035cb8d78870792b837299c
Inyuki <m.indriunas@universians.org>
2010-09-04 18:42:54 -0500

    Trying to create a table, add some data, and query the database.

--- yukia.py
+++ yukia.py
@@ -1,3 +1,14 @@
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://inyuki_yukia:7cd26db5@localhost/inyuki_yukia', echo=True)
 
+result = engine.execute('''DROP TABLE units''')
+result = engine.execute('''CREATE TABLE units (id serial, unit varchar(40) NOT NULL);''')
+result = engine.execute('''INSERT INTO units VALUES (1, 'Bricks laid');''')
+result = engine.execute('''SELECT * FROM units;''')
+
+for row in result:
+    print "unit:", row['unit']
+
+result = engine.execute('''DROP TABLE units;''')
+result.close()
+
