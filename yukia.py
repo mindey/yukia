4c3fd72c1a2ad34cd19e39e4d52ca1a84fbfa71d
Inyuki <m.indriunas@universians.org>
2010-09-05 14:17:19 -0500

    Added "stocks", "names" and "items" tables to the db model.

--- yukia.py
+++ yukia.py
@@ -1,8 +1,28 @@
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://inyuki_yukia:7cd26db5@localhost/inyuki_yukia', echo=True)
 from sqlalchemy import MetaData
-meta_data = MetaData()
 from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import mapper, relation, backref
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('postgresql://inyuki_yukia:7cd26db5@localhost/inyuki_yukia', echo=False)
+meta_data = MetaData()
+
+# Creating database model
+
+stocks_table = Table('stocks', meta_data,
+    Column('sid', Integer, primary_key=True),
+    Column('name', String(30)),
+)
+
+names_table = Table('names', meta_data,
+    Column('nid', Integer, primary_key=True),
+    Column('name', String(40)),
+)
+
+items_table = Table('items', meta_data,
+    Column('iid', Integer, primary_key=True),
+    Column('nid', Integer, ForeignKey('names.nid')),
+    Column('sid', Integer, ForeignKey('stocks.sid')),
+)
 
 units_table = Table('units', meta_data,
     Column('id', Integer, primary_key=True),
@@ -11,6 +31,8 @@ units_table = Table('units', meta_data,
 
 meta_data.create_all(engine)
 
+# Creating and mapping OO model to the database model
+
 class Unit(object):
     def __init__(self, unit):
         self.unit = unit
@@ -18,19 +40,23 @@ class Unit(object):
     def __repr__(self):
         return "" % (self.unit,)
 
-from sqlalchemy.orm import mapper, relation, backref
 mapper(Unit, units_table)
 
-from sqlalchemy.orm import sessionmaker
+# Creating and adding some objects to the database 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 brick = Unit('bricks laid')
 tile = Unit('roof tiles mounted')
 units_list = [brick, tile]
+
 session.add_all(units_list)
 session.commit()
 
+# Querying the database (should be)
+
+
+# Printing some ojbects from the database (should be) 
 for u in units_list:
     print "Unit:\t", u.unit
 
