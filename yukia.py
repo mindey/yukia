102905b68a6d3d603beb3f381a917c65cf2d7b16
Inyuki <m.indriunas@universians.org>
2010-09-04 19:59:13 -0500

    Created equivalent model for the table with SQLAlchemy, without directly querying db; a little messy, but seem to work.

--- yukia.py
+++ yukia.py
@@ -1,14 +1,36 @@
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://inyuki_yukia:7cd26db5@localhost/inyuki_yukia', echo=True)
+from sqlalchemy import MetaData
+meta_data = MetaData()
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
 
-result = engine.execute('''DROP TABLE units''')
-result = engine.execute('''CREATE TABLE units (id serial, unit varchar(40) NOT NULL);''')
-result = engine.execute('''INSERT INTO units VALUES (1, 'Bricks laid');''')
-result = engine.execute('''SELECT * FROM units;''')
+units_table = Table('units', meta_data,
+    Column('id', Integer, primary_key=True),
+    Column('unit', String(40)),
+)
 
-for row in result:
-    print "unit:", row['unit']
+meta_data.create_all(engine)
 
-result = engine.execute('''DROP TABLE units;''')
-result.close()
+class Unit(object):
+    def __init__(self, unit):
+        self.unit = unit
+
+    def __repr__(self):
+        return "" % (self.unit,)
+
+from sqlalchemy.orm import mapper, relation, backref
+mapper(Unit, units_table)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+
+brick = Unit('bricks laid')
+tile = Unit('roof tiles mounted')
+units_list = [brick, tile]
+session.add_all(units_list)
+session.commit()
+
+for u in units_list:
+    print "Unit:\t", u.unit
 
