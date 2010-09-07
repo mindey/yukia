2461d930b05d2f6d8029f83e50dc30cc4b7ae0ef
Inyuki <m.indriunas@universians.org>
2010-09-07 09:34:35 -0500

    Changed the tables to declarative. Added units table.

--- yukia.py
+++ yukia.py
@@ -1,71 +1,86 @@
-from sqlalchemy import create_engine
-from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import mapper, relation, backref
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relation, sessionmaker
+
+Base = declarative_base()
+
+class Stock(Base):
+    __tablename__ = 'stocks'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+
+    def __init__(self, name=None):
+        self.name = name
+    def __repr__(self):
+        return "Stock(%r)" % (self.name)
+
+class Name(Base):
+    __tablename__ = 'names'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40))
+
+    def __init__(self, name=None):
+        self.name = name
+    def __repr__(self):
+        return "Name(%r)" % (self.name)
+
+class Label(Base):
+    __tablename__ = 'labels'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40))
+
+    def __init__(self, name=None):
+        self.name = name
+    def __repr__(self):
+        return "Label(%r)" % (self.name)
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    named_by = Column(Integer, ForeignKey('names.id'))
+    stocked_at = Column(Integer, ForeignKey('stocks.id'))
+    labeled_with = Column(Integer, ForeignKey('labels.id'))
+
+    name = relation("Name", backref='items', lazy=False)
+    stock = relation("Stock", backref='items', lazy=False)
+    label = relation("Label", backref='items', lazy=False)
+
+    def __init__(self):
+        pass
+    def __repr__(self):
+        return "Item(%r, %r, %r)" % (self.name, self.stock, self.label)
+
+class Process(Base):
+    __tablename__ = 'processes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40))
+    owned_by = Column(Integer, ForeignKey('items.id'))
+
+    item = relation("Item", backref='processes', lazy=False)
+
+    def __init__(self, name=None):
+        self.name = name
+    def __repr__(self):
+        return "Process(%r, %r)" % (self.name, self.item)
+
+class Unit(Base):
+    __tablename__ = 'units'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40))
+    owned_by = Column(Integer, ForeignKey('processes.id'))
+
+    process = relation("Process", backref='units', lazy=False)
+
+    def __init__(self, name=None):
+        self.name = name
+    def __repr__(self):
+        return "Unit(%r, %r)" % (self.name, self.process)
+
 engine = create_engine('postgresql://inyuki_yukia:7cd26db5@localhost/inyuki_yukia', echo=False)
-meta_data = MetaData()
-
-meta_data.drop_all(engine)
-
-# Creating database model
-
-stocks_table = Table('stocks', meta_data,
-    Column('sid', Integer, primary_key=True),
-    Column('name', String(30)),
-)
-
-names_table = Table('names', meta_data,
-    Column('nid', Integer, primary_key=True),
-    Column('name', String(40)),
-)
-
-items_table = Table('items', meta_data,
-    Column('iid', Integer, primary_key=True),         # item id (unique)
-    Column('nid', Integer, ForeignKey('names.nid')),  # name id
-    Column('sid', Integer, ForeignKey('stocks.sid')),  # stock id
-    Column('lid', Integer, ForeignKey('labels.lid')), # label id
-) 
-
-labels_table = Table('labels', meta_data,
-    Column('lid', Integer, primary_key=True),
-    Column('name', String(30)),
-)
-
-processes_table = Table('processes', meta_data,
-    Column('pid', Integer, primary_key=True),
-    Column('name', String(40)),
-    Column('item', Integer, ForeignKey('items.iid')), # producer id ( a kind of label for process )
-)
-
-meta_data.create_all(engine)
-
-# Creating and mapping OO model to the database model
-
-#class Unit(object):
-#    def __init__(self, unit):
-#        self.unit = unit
-#
-#    def __repr__(self):
-#        return "" % (self.unit,)
-
-#mapper(Unit, units_table)
-
-# Creating and adding some objects to the database 
-#Session = sessionmaker(bind=engine)
-#session = Session()
-
-#brick = Unit('bricks laid')
-#tile = Unit('roof tiles mounted')
-#units_list = [brick, tile]
-
-#session.add_all(units_list)
-#session.commit()
-
-# Querying the database (should be)
-
-
-# Printing some ojbects from the database (should be) 
-#for u in units_list:
-#    print "Unit:\t", u.unit
-
+Base.metadata.create_all(engine)
