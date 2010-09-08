7f6f73a261be7dde1b8da063d20a2b24a36f0105
Inyuki <m.indriunas@universians.org>
2010-09-08 18:10:52 -0500

    Added Markets and "Sources" -- prototypes of Items.

--- yukia.py
+++ yukia.py
@@ -40,7 +40,7 @@ class Name(Base):                      # Particular names, i.e., "McChicken"
     # So, an item name is fully specified by Type and Name, i.e., [Sandwitch][McChiken]
 
 
-class Label(Base):      # Labels will be used to categorize Items.
+class Label(Base):      # To be used to sort Items into "Producers", "Raw Materials", "Currencies" and other functional kinds.
     __tablename__ = 'labels'
 
     id = Column(Integer, primary_key=True)
@@ -55,8 +55,8 @@ class Item(Base):
     __tablename__ = 'items'
 
     id = Column(Integer, primary_key=True)
-    typed_as = Column(Integer, ForeignKey('types.id'), nullable=False, unique=True)  # i.e., "Sandwitch"
-    named_by = Column(Integer, ForeignKey('names.id'), nullable=True, unique=True)  # i.e., "McChicken"
+    typed_as = Column(Integer, ForeignKey('types.id'), nullable=False)  # i.e., "Sandwitch"
+    named_by = Column(Integer, ForeignKey('names.id'), nullable=True)  # i.e., "McChicken"
     stocked_at = Column(Integer, ForeignKey('stocks.id'), nullable=False)   # i.e., London, New York
     labeled_with = Column(Integer, ForeignKey('labels.id'), nullable=False) # i.e., Producer, Raw material, Currency
 
@@ -111,7 +111,7 @@ class Performance(Base):
     unit = relation("Unit", backref='units', lazy=False)
 
     def __init__(self, hour=0, total=0):
-        self.id = int(time.time())/3600 # As perofrmance is measured, the hour id is calculated.
+        self.hour = int(time.time())/3600 # As perofrmance is measured, the hour id is calculated.
         self.total = total
     def __repr__(self):
         return "Performance(%r, %r, %r)" % (self.total, self.process, self.unit)
@@ -119,6 +119,33 @@ class Performance(Base):
     # No need for frequent cron jobs, because the time when the object was created is memorized.
     # One cron job in the end of the working day is sufficient to include the last hour data of the day.
 
+class Market(Base):
+    __tablename__ = 'markets'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False, unique=True)
+
+    def __init__(self, name='Home'):
+        self.name = name
+    def __repr__(self):
+        return "Market(%r)" % (self.name)
+
+class Source(Base):      # Prototype for issuing Items.
+    __tablename__ = 'sources'
+
+    id = Column(Integer, primary_key=True)
+    typed_as = Column(Integer, ForeignKey('types.id'), nullable=False)  # i.e., "Sandwitch"
+    named_by = Column(Integer, ForeignKey('names.id'), nullable=True)  # i.e., "McChicken"
+    marketed_in = Column(Integer, ForeignKey('markets.id')) # Thought: MarketID = 1 -> Home 
+    labeled_with = Column(Integer, ForeignKey('labels.id'), nullable=False) # i.e., Producer, Raw material, Currency
+    # quantity = Column(Integer, nullable=False) # Temporarily considered to be limitless.
+
+    market = relation("Market", backref='sources', lazy=False)
+ 
+    def __init__(self, name='Home'):
+        pass 
+    def __repr__(self):
+        return "Source(%r)" % (self.name) 
 
 engine = create_engine('sqlite:///yukia.db', echo=False)
 Base.metadata.create_all(engine)
